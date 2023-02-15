ebafc4af94aa7ca624a9510d232f4d94cfe6c066
thiago <tmg3.mn@gmail.com>
2023-02-15 10:26:37 -0300

    Add routes to root component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.scss';
+import Create from './pages/create/Create';
+import Home from './pages/home/Home';
+import Recipe from './pages/recipe/Recipe';
+import Search from './pages/search/Search';
 
 function App() {
 
   return (
     <div className="App">
-      Cooking Site
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/create' element={<Create />} />
+          <Route path='/search' element={<Search />} />
+          <Route path='/recipes/:id' element={<Recipe />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
