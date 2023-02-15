bf8020c361ac2a37f5442fa5cd69b59684a0d761
thiago <tmg3.mn@gmail.com>
2023-02-15 12:37:00 -0300

    Add HTML and styles to home page component

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import useFetch from '../../hooks/useFetch'
 import './home.scss'
 
+interface RecipeInfo {
+  id: number;
+  title: string;
+}
+
 const Home = () => {
+
+  const { data, isPending, error } = useFetch<RecipeInfo[]>('http://localhost:3000/recipes')
+
   return (
-    <div>Home</div>
+    <div className='home'>
+      {error && <p className='error'>{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {data &&
+        data.map((recipe) => (
+          <h2 key={recipe.id}>{recipe.title}</h2>
+        ))
+      }
+    </div>
   )
 }
 
