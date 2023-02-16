2159d5c50902e4f50252d9ca0a674f1507140ec6
thiago <tmg3.mn@gmail.com>
2023-02-16 10:13:23 -0300

    Add styles, JSX, and TS to recipe page component

--- src/pages/recipe/Recipe.tsx
+++ src/pages/recipe/Recipe.tsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
+import useFetch from '../../hooks/useFetch';
 import './recipe.scss';
 
+interface SingleRecipeData {
+  title: string;
+  cookingTime: string;
+  ingredients: string[];
+  method: string;
+}
+
 const Recipe = () => {
+
+  const { id } = useParams<{ id: string }>()
+  const url = 'http://localhost:3000/recipes/' + id
+  const { error, isPending, data: recipe } = useFetch<SingleRecipeData>(url)
+
   return (
-    <div>Recipe</div>
+    <div className='recipe'>
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {recipe && 
+        <>
+          <h2 className="page-title">{recipe.title}</h2>
+          <p>Takes {recipe.cookingTime} to cook.</p>
+          <ul>
+            {recipe.ingredients.map((ing, index) => (
+              <li key={index}>{ing}</li>
+            ))}
+          </ul>
+          <p className="method">{recipe.method}</p>
+        </>
+      }
+    </div>
   )
 }
 
