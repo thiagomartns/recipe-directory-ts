29eaa7cb81b0a1fbc95bd0d5d1cecfaf11a603ce
thiago <tmg3.mn@gmail.com>
2023-02-16 10:49:53 -0300

    Add styles, JSX, and TS to create page component

--- src/pages/create/Create.tsx
+++ src/pages/create/Create.tsx
@@ -1,9 +1,50 @@
-import React from 'react'
-import './Create.scss';
+import React, { FormEvent, useState } from 'react'
+import './create.scss';
 
 const Create = () => {
+
+  const [title, setTitle] = useState<string>('')
+  const [method, setMethod] = useState<string>('')
+  const [cookingTime, setCookingTime] = useState<number | string>('')
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault()
+    console.log(title, method, cookingTime);
+  }
+
   return (
-    <div>Create</div>
+    <div className='create'>
+      <h2 className="page-title">Add a new recipe</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Recipe Title:</span>
+          <input 
+            type='text'
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+            required
+          />
+        </label>
+        <label>
+          <span>Recipe Method:</span>
+          <textarea 
+            onChange={(e) => setMethod(e.target.value)}
+            value={method}
+            required
+          />
+        </label>
+        <label>
+          <span>Cooking Time (minutes):</span>
+          <input
+            type='number' 
+            onChange={(e) => setCookingTime(e.target.value)}
+            value={cookingTime}
+            required
+          />
+        </label>
+        <button className="btn">submit</button>
+      </form>
+    </div>
   )
 }
 
