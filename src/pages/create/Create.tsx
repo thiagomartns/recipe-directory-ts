f7b1724852ca2e57b68b4cc54e7c5f56832e2672
thiago <tmg3.mn@gmail.com>
2023-02-27 10:30:27 -0300

    Add function to ingredients from input

--- src/pages/create/Create.tsx
+++ src/pages/create/Create.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent, useState } from 'react'
+import React, { FormEvent, useRef, useState } from 'react'
 import './create.scss';
 
 const Create = () => {
@@ -6,10 +6,27 @@ const Create = () => {
   const [title, setTitle] = useState<string>('')
   const [method, setMethod] = useState<string>('')
   const [cookingTime, setCookingTime] = useState<number | string>('')
+  const [newIngredient, setNewIngredient] = useState<string>('')
+  const [ingredients, setIngredients] = useState<string[]>([])
+  const ingredientInput = useRef<null | HTMLInputElement>(null);
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault()
-    console.log(title, method, cookingTime);
+    console.log(title, method, cookingTime, ingredients);
+  }
+
+  const handleAdd = (e: FormEvent) => {
+    e.preventDefault();
+    const ing = newIngredient.trim();
+
+    if (ing && !ingredients.includes(ing)) {
+      setIngredients(previIngredients => [...previIngredients, newIngredient])
+    }
+
+    setNewIngredient('')
+    if (ingredientInput.current) {
+      ingredientInput.current.focus();
+    }
   }
 
   return (
@@ -25,6 +42,23 @@ const Create = () => {
             required
           />
         </label>
+
+        <label>
+          <span>Recipe Ingredients:</span>
+          <div className="ingredients">
+            <input 
+              type="text" 
+              onChange={(e) => setNewIngredient(e.target.value)}
+              value={newIngredient}
+              ref={ingredientInput}
+            />
+            <button onClick={handleAdd} className="btn">add</button>
+          </div>
+        </label>
+        <p>Current ingredients: {ingredients.map((i, index) => (
+          <em key={index}>{i}, </em>
+        ))}</p>
+
         <label>
           <span>Recipe Method:</span>
           <textarea 
